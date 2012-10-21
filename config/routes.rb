54fd8b20f5844df32b05932207e69331d3a690ea
Chris Smyth <chris.smyth@intraincorp.com.au>
2012-10-21 02:56:37 -0700

    moved root :to => "movies#index" to top of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rottenpotatoes::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "movies#index"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -49,7 +51,12 @@ Rottenpotatoes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
+ # match '/movies' => "movies#index"
+ # match "/" => redirect("/movies")
+
+  #root :to => MovieController.action(:index)
+
 
   # See how all your routes lay out with "rake routes"
 
